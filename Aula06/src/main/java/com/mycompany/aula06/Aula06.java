c600264bee2c1448039a148c432528d50cd321d9
Caleb Wacht <calebwacht@gmail.com>
2023-08-28 14:56:03 -0300

    Aula06

---
+++ Aula06/src/main/java/com/mycompany/aula06/Aula06.java
@@ -0,0 +1,17 @@
+package com.mycompany.aula06;
+
+/**
+ *
+ * @author Caleb Wacht
+ */
+public class Aula06 {
+
+    public static void main(String[] args) {
+        ControleRemoto c = new ControleRemoto();
+        c.ligar();
+        c.ligarMudo();
+        c.play();
+        c.abrirMenu();
+        c.fecharMenu();
+    }
+}
---
+++ Aula06/src/main/java/com/mycompany/aula06/Controlador.java
@@ -0,0 +1,20 @@
+package com.mycompany.aula06;
+
+/**
+ *
+ * @author Caleb Wacht
+ */
+public interface Controlador {
+    
+    //Métodos abstratos
+    public abstract void ligar();
+    public abstract void desligar();
+    public abstract void abrirMenu();
+    public abstract void fecharMenu();
+    public abstract void maisVolume();
+    public abstract void menosVolume();
+    public abstract void ligarMudo();
+    public abstract void desligarMudo();
+    public abstract void play();
+    public abstract void pause();
+}
